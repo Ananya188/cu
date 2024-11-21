565a1b567da2b834c047f082e0f0509bff66ef94
Ananya Dutta <60771644+Ananya188@users.noreply.github.com>
2024-11-21 18:24:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="heading">
+        <h1>Currency</h1>
+      </div>
+    </div>
+  );
+}
